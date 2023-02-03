e29d2c849e516c17d842e7ceaf69ebc8c32bfbe4
carlosil <carlosgarciagil983@gmail.com>
2023-02-03 20:15:13 +0100

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { YourBooksComponent } from './your-books/your-books.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component:HomeComponent},
+  { path: 'home', component:HomeComponent},
+  { path: 'yourBooks', component:YourBooksComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { YourBooksComponent } from './your-books/your-books.component';
+import { MenuComponent } from './menu/menu.component';
+import { FooterComponent } from './footer/footer.component';
+import { CardComponent } from './card/card.component';
+import { ButtonComponent } from './button/button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    
+    HeaderComponent,
+         HomeComponent,
+         YourBooksComponent,
+         MenuComponent,
+         FooterComponent,
+         CardComponent,
+         ButtonComponent,
   ],
   imports: [
     BrowserModule,
